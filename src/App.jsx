5028e9494f62edc9b3313c8b010ffe51a475c25c
Mauro van der Duim <mauro.van.der.duim.van.der.duim@hva.nl>
2026-04-08 15:55:48 +0200

    Remove unused assets and clean up styles and components
    - Deleted various image assets including logos and GIFs.
    - Removed CSS files and styles related to About, Contact, Experience, Home, and Projects pages.
    - Cleaned up the Experience and Home components by removing unnecessary imports and styles.
    - Updated main.jsx to use StrictMode and removed BrowserRouter.
    - Simplified index.css by removing unused styles and resetting margins and paddings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
-import './App.css'
-import Nav from './Nav.jsx'
-import Home from './pages/Home.jsx'
-import Projects from './pages/Projects.jsx'
-import Contact from './pages/Contact.jsx'
-import About from './pages/About.jsx'
-import Experience from './pages/Experience.jsx'
 
 function App() {
-  const location = useLocation()
-
   return (
-    <main className="portfolio">
-      <Nav />
-      <div key={location.pathname} className="page-transition">
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="/experience" element={<Experience />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </main>
+    <div>
+      <h1>Milou is de beste in de wereld</h1>
+
+    </div>
   )
 }
 
